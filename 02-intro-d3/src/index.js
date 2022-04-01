3117f7777af88e7f977e50ec758e985951680307
Marilyn Themo <marilyn.themo@heig-vd.ch>
2022-04-01 21:53:13 +0200

    update src

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -12,6 +12,7 @@ const svg = d3.select("body")
 const groupe1 = svg.append("g")
 const groupe2 = svg.append("g")
 const groupe3 = svg.append("g")
+const groupe4 = svg.append("g")
 
 
 //// Groupe - 1
@@ -19,7 +20,7 @@ groupe1.append("circle")
     .attr("cx", "50")
     .attr("cy", "50")
     .attr("r", "40")
-    .attr("transform", "translate (50,0)")
+    .attr("transform", "translate (50,0)") //à la base les cercles étaients alignés
 
 groupe1.append("text")
     .text("Marilyn")
@@ -55,7 +56,7 @@ groupe3.append("text")
 //Alignement si click
 
 groupe3.on("click", () => {
-    groupe1.attr("transform", "translate (200,0)") //On doit faire translate si on veut bouger tout le groupe
+    groupe1.attr("transform", "translate (200,0)") //On doit faire translate si on veut bouger tout le groupe (qui a déjà été translaté)
     groupe2.attr("transform", "translate (100,0)")
     console.log("coucou");
 })
@@ -66,20 +67,28 @@ groupe3.on("click", () => {
 
 const data = [20, 5, 25, 8, 15]
 
-const svgRect = d3.select("body")
-    .append("svg")
-
-svgRect.selectAll("rect") //Même si ils n'existent pas encore, je vais les créer après
+groupe4.selectAll("rect") //Même si ils n'existent pas encore, je vais les créer après
     .data(data)
     .enter() //Pour faire entrer les données dans la fonction
-
     .append("rect")
+    
+        //Définir la position xy de chaque colonne
+        .attr('x', (d, i) => i * 30) //Pour chaque index, la valeur du x = 0*30...1*30...
+                //Ne pas oublier que l'origine est en haut à gauche et que le positif tend vers le bas. 
+        //Donc pour faker que c'est à l'endroit notre y doit commencer un peu plus bas soit : height - d value 
+        .attr('y', d => HEIGHT - d) //Comme la longueur du graph + height = ça arrivera toujours au même point.
+
+        //Définir la largeur et la hauteur pour qu'il y ait apparition de colonnes
+
     .attr('height', d => d) //Hauteur de la colonne relative aux cases du tableau
     .attr('width', "20") //Largeur de la colonne
 
-    //Définir la position xy de chaque colonne
-    .attr('x', (d, i) => i * 30) //Pour chaque index, la valeur du x = 0*30...1*30...
-    .attr('y', d => 0) //On se base sur la hauteur de la colonne et la position y est à sa source (d-d) =  0
+
+
+    //Utiliser les groupes 
+    //Utiliser les attributs 
+    //On peut faire bouger les choses au clic avec le transform
+
 
 
 
